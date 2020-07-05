41e1a858c4455285a14f840c2f584795580acb84
Xavier Claude <contact@xavierclaude.be>
2020-07-05 16:31:32 +0200

    Handling error with approximation

--- updateEM.py
+++ updateEM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ Script to run to update euromussels cookie """
 import os
+import time
 import requests
 
 import django
@@ -10,18 +11,25 @@ django.setup()
 
 from boardauth.models import EuroLogin
 
+def updateCookie(eurologin):
+    session = requests.session()
+    req = session.post(
+        'https://faab.euromussels.eu/loginA.php',
+        data={'login': eurologin.username, 'password': eurologin.password,
+        'action': 'Login'})
+    cookie = session.cookies['faab_id']
+    eurologin.cookie = cookie
+    eurologin.save()
 
 def main():
     """ Connect to euromussels and request a new cookie """
     for eurologin in EuroLogin.objects.all():
-        session = requests.session()
-        req = session.post(
-            'https://faab.euromussels.eu/loginA.php',
-            data={'login': eurologin.username, 'password': eurologin.password,
-            'action': 'Login'})
-        cookie = session.cookies['faab_id']
-        eurologin.cookie = cookie
-        eurologin.save()
+        try:
+            updateCookie(eurologin)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            updateCookie(eurologin)
 
 
 if __name__ == '__main__':
